f56687f0c07646ca3835dfaf0d2abb09865a9183
Sopa89 <tc689@drexel.edu>
2019-06-28 20:36:48 -0400

    More ban changes
    Checked role positions before banning

--- cmds/ban.js
+++ cmds/ban.js
@@ -8,12 +8,16 @@ module.exports.run = async (bot, message, args) => {
 
     if (!message.member.hasPermission('BAN_MEMBERS')) return message.reply('you do not have permissions to use this command!s');
     if (!target) return message.reply('please specify a member to ban!');
+
+    if (target.highestRole.position >= message.guild.member(message.author).highestRole.position && message.guild.owner != message.member) return message.channel.send(`You cannot ban a member who is higher or has the same role as you.`)
+
+    if(target.highestRole.position >= message.guild.member(bot.user).highestRole.position) return message.channel.send(`I cannot ban a member who is higher or has the same role as me.`);
+
     if (!reason) return message.reply('please specify a reason for this ban!');
 
-    let dm = target.user.dmChannel;
-    if (!dm) dm = await target.createDM();
+    let dm = await target.createDM();
 
-    await dm.send(`Hello, you have been banned from ${message.guild.name} for: ${reason}`);
+    dm.send(`Hello, you have been banned from ${message.guild.name} for: ${reason}`);
 
     let embed = new discord.RichEmbed()
         .setColor('RANDOM')
@@ -26,7 +30,7 @@ module.exports.run = async (bot, message, args) => {
         .setFooter('Banned user information', target.user.displayAvatarURL);
 
     message.channel.send(embed);
-    target.ban(reason);
+    return await target.ban(reason);
 };
 
 module.exports.config = {
